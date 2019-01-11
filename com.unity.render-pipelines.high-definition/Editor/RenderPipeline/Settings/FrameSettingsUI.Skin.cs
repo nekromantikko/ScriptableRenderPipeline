bfc3ddb2c40a2849d27999d248fb85002c4a3d27
JordanL8 <jordanl8@uni.coventry.ac.uk>
2019-01-11 17:21:41 +0000

    Started Frame Settings Tooltips

--- com.unity.render-pipelines.high-definition/Editor/RenderPipeline/Settings/FrameSettingsUI.Skin.cs
+++ com.unity.render-pipelines.high-definition/Editor/RenderPipeline/Settings/FrameSettingsUI.Skin.cs
@@ -11,20 +11,20 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
         const string lightSettingsHeaderContent = "Lighting";
         const string asyncComputeSettingsHeaderContent = "Async Compute";
         
-        static readonly GUIContent transparentPrepassContent = CoreEditorUtils.GetContent("Transparent Prepass");
-        static readonly GUIContent transparentPostpassContent = CoreEditorUtils.GetContent("Transparent Postpass");
-        static readonly GUIContent motionVectorContent = CoreEditorUtils.GetContent("Motion Vectors");
-        static readonly GUIContent objectMotionVectorsContent = CoreEditorUtils.GetContent("Object Motion Vectors");
-        static readonly GUIContent decalsContent = CoreEditorUtils.GetContent("Decals");
-        static readonly GUIContent roughRefractionContent = CoreEditorUtils.GetContent("Rough Refraction");
-        static readonly GUIContent distortionContent = CoreEditorUtils.GetContent("Distortion");
-        static readonly GUIContent postprocessContent = CoreEditorUtils.GetContent("Postprocess");
-        static readonly GUIContent litShaderModeContent = CoreEditorUtils.GetContent("Lit Shader Mode");
+        static readonly GUIContent transparentPrepassContent = CoreEditorUtils.GetContent("Transparent Prepass|When enabled, Cameras using these Frame Settings process a transparent prepass.");
+        static readonly GUIContent transparentPostpassContent = CoreEditorUtils.GetContent("Transparent Postpass|When enabled, Cameras using these Frame Settings process a transparent postpass.");
+        static readonly GUIContent motionVectorContent = CoreEditorUtils.GetContent("Motion Vectors|When enabled, Cameras using these Frame Settings process a motion vector pass.");
+        static readonly GUIContent objectMotionVectorsContent = CoreEditorUtils.GetContent("Object Motion Vectors||When enabled, Cameras using these Frame Settings process a object motion vector pass.");
+        static readonly GUIContent decalsContent = CoreEditorUtils.GetContent("Decals|When enabled, Cameras using these Frame Settings process a decal render pass.");
+        static readonly GUIContent roughRefractionContent = CoreEditorUtils.GetContent("Rough Refraction|When enabled, Cameras using these Frame Settings process a rough refraction render pass.");
+        static readonly GUIContent distortionContent = CoreEditorUtils.GetContent("Distortion|When enabled, Cameras using these Frame Settings process a distortion render pass.");
+        static readonly GUIContent postprocessContent = CoreEditorUtils.GetContent("Postprocess|When enabled, Cameras using these Frame Settings process a postprocessing render pass.");
+        static readonly GUIContent litShaderModeContent = CoreEditorUtils.GetContent("Lit Shader Mode|Specifies the Lit Shader Mode Cameras using these Frame Settings use to render the Scene.");
         static readonly GUIContent depthPrepassWithDeferredRenderingContent = CoreEditorUtils.GetContent("Depth Prepass With Deferred Rendering");
         static readonly GUIContent opaqueObjectsContent = CoreEditorUtils.GetContent("Opaque Objects");
         static readonly GUIContent transparentObjectsContent = CoreEditorUtils.GetContent("Transparent Objects");
         static readonly GUIContent realtimePlanarReflectionContent = CoreEditorUtils.GetContent("Enable Realtime Planar Reflection"); 
-        static readonly GUIContent msaaContent = CoreEditorUtils.GetContent("MSAA");
+        static readonly GUIContent msaaContent = CoreEditorUtils.GetContent("MSAA|When enabled, Cameras using these Frame Settings calculate MSAA when they render the Scene.");
         static readonly GUIContent shadowContent = CoreEditorUtils.GetContent("Shadow");
         static readonly GUIContent contactShadowContent = CoreEditorUtils.GetContent("Contact Shadows");
         static readonly GUIContent shadowMaskContent = CoreEditorUtils.GetContent("Shadow Masks");
@@ -46,6 +46,6 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
         static readonly GUIContent volumeVoxelizationAsyncContent = CoreEditorUtils.GetContent("Volumetrics Voxelization in Async");
 
 
-        static readonly GUIContent frameSettingsHeaderContent = CoreEditorUtils.GetContent("Frame Settings Overrides|Default FrameSettings are defined in HDRenderPipelineAsset.");
+        static readonly GUIContent frameSettingsHeaderContent = CoreEditorUtils.GetContent("Frame Settings Overrides|Default FrameSettings are defined in your Unity Project's HDRP Asset.");
     }
 }
